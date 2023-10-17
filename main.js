c6621da2b0a5b229ce75a4f6e4ce09e805795876
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-17 13:31:08 +0530

    added play pause functionality

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 const songs = document.querySelectorAll('audio')
-const playButtons = document.querySelectorAll('button')
+const playButtons = document.querySelectorAll('button i')
 // const pauseButtons = document.querySelectorAll('pauseBtn')
 const log = console.log
 
 function stopSongs() {
     songs.forEach(song => {
+        playButtons.forEach(btn => {
+            btn.classList.add('fa-play');
+            btn.classList.remove('fa-pause')
+        })
         song.pause()
     })
 }
 
 playButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        stopSongs()
-        const song = document.getElementById(btn.id.slice(0, -3))
-        log(btn.classList.contains('fa-play'))
-        song.play()
-        e.target.classList.toggle('fa-play')
-        e.target.classList.toggle('fa-pause')
+        if (e.target.classList.contains('fa-play')) {
+            stopSongs()
+            const song = document.getElementById(btn.parentElement.id.slice(0, -3))
+            song.play()
+            btn.classList.remove('fa-play');
+            btn.classList.add('fa-pause')
+        }
+        else if (e.target.classList.contains('fa-pause')) {
+            const song = document.getElementById(btn.parentElement.id.slice(0, -3))
+            song.pause()
+            btn.classList.add('fa-play');
+            btn.classList.remove('fa-pause')
+        }
+
 
     })
 })
 
 
-// pauseButtons.forEach(btn => {
-//     btn.addEventListener('click', (e) => {
-//         console.log('clicked')
-
-//         stopSongs()
-//     })
-// })
 
