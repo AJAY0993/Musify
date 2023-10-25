dce2af88c4db9dd030267f018fe7d1e2dcd91a1c
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-25 17:06:43 +0530

    added progress bar

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const songs = document.querySelectorAll('audio')
-const playButtons = document.querySelectorAll('button i')
+const playButtons = document.querySelectorAll('main button i')
 const duration = document.querySelectorAll('.audio-length')
+const toolbarBtns = document.querySelectorAll('.toolbar button')
+const progressBar = document.querySelector('.progress-bar')
+const pauseItBtn = document.querySelector('#pause-it').firstElementChild
 const log = console.log
-
+let playingSong;
+let playingSongIndex = 0;
 duration.forEach((songDuration, i) => {
-    songDuration.innerText = songs[i].duration
+    songDuration.innerText = (songs[i].duration / 60).toFixed(2) + 'min'
 })
+
+
 
 function stopSongs() {
     songs.forEach(song => {
@@ -23,12 +29,21 @@ playButtons.forEach(btn => {
             stopSongs()
             const song = document.getElementById(btn.parentElement.id.slice(0, -3))
             song.play()
+            playingSong = song
+            playingSong.addEventListener('timeupdate', () => {
+                progressBar.value = (playingSong.currentTime / playingSong.duration) * 100
+            })
+            pauseItBtn.classList.remove('fa-play')
+            pauseItBtn.classList.add('fa-pause')
+
             btn.classList.remove('fa-play');
             btn.classList.add('fa-pause')
         }
         else if (e.target.classList.contains('fa-pause')) {
             const song = document.getElementById(btn.parentElement.id.slice(0, -3))
             song.pause()
+            pauseItBtn.classList.remove('fa-pause')
+            pauseItBtn.classList.add('fa-play')
             btn.classList.add('fa-play');
             btn.classList.remove('fa-pause')
         }
@@ -38,4 +53,26 @@ playButtons.forEach(btn => {
 })
 
 
+toolbarBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        switch (btn.id) {
+            case 'play-prev':
+                break;
+            case 'pause-it':
+                if (btn.firstElementChild.classList.contains('fa-pause')) {
+                    playingSong.pause()
+                    pauseItBtn.remove('fa-pause')
+                    pauseItBtn.add('fa-play')
+                }
+                else {
+                    playingSong.play()
+                    pauseItBtn.remove('fa-play')
+                    pauseItBtn.add('fa-pause')
+                }
+                break;
+            case 'play-next':
+                console.log('play-next')
+        }
+    })
+})
 
