2643483a3501b5a6a39fc92e2b9c7fbb45641772
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-25 17:25:39 +0530

    did some changes

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const songs = document.querySelectorAll('audio')
+const songs = Array.from(document.querySelectorAll('audio'))
 const playButtons = document.querySelectorAll('main button i')
 const duration = document.querySelectorAll('.audio-length')
 const toolbarBtns = document.querySelectorAll('.toolbar button')
 const progressBar = document.querySelector('.progress-bar')
 const pauseItBtn = document.querySelector('#pause-it').firstElementChild
 const log = console.log
-let playingSong;
-let playingSongIndex = 0;
+let playingSong = songs[0];
+let playingSongIndex = songs.indexOf(playingSong);
+
 duration.forEach((songDuration, i) => {
     songDuration.innerText = (songs[i].duration / 60).toFixed(2) + 'min'
 })
@@ -48,7 +49,7 @@ playButtons.forEach(btn => {
             btn.classList.remove('fa-pause')
         }
 
-
+        playingSongIndex = songs.indexOf(playingSong);
     })
 })
 
@@ -57,21 +58,26 @@ toolbarBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         switch (btn.id) {
             case 'play-prev':
+                stopSongs()
+                if (playingSongIndex > 0) songs[playingSongIndex - 1].play();
+                songs[songs.length - 1].play()
                 break;
             case 'pause-it':
                 if (btn.firstElementChild.classList.contains('fa-pause')) {
                     playingSong.pause()
-                    pauseItBtn.remove('fa-pause')
-                    pauseItBtn.add('fa-play')
+                    pauseItBtn.classList.remove('fa-pause')
+                    pauseItBtn.classList.add('fa-play')
                 }
                 else {
                     playingSong.play()
-                    pauseItBtn.remove('fa-play')
-                    pauseItBtn.add('fa-pause')
+                    pauseItBtn.classList.remove('fa-play')
+                    pauseItBtn.classList.add('fa-pause')
                 }
                 break;
             case 'play-next':
-                console.log('play-next')
+                stopSongs()
+                if (playingSongIndex < songs.length - 1) songs[playingSongIndex + 1].play();
+                songs[0].play()
         }
     })
 })
